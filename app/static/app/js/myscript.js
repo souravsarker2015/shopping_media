07c61c52a842c7731fedd717737e12b8967e74f9
sourov-sarker8251 <souravsarker2015@gmail.com>
2022-01-25 12:18:30 +0600

    add-to-cart updated

--- app/static/app/js/myscript.js
+++ app/static/app/js/myscript.js
@@ -21,3 +21,61 @@ $('#slider1, #slider2, #slider3').owlCarousel({
         }
     }
 })
+
+$('.plus-cart').click(function () {
+    var id = $(this).attr("pid").toString();
+    var eml = this.parentNode.children[2]
+    // console.log(id)
+    $.ajax({
+        type: 'GET',
+        url: '/plus_cart',
+        data: {
+            prod_id: id
+        },
+        success: function (data) {
+            eml.innerText = data.quantity
+            document.getElementById("amount").innerText = data.amount
+            document.getElementById("total").innerText = data.total
+
+        }
+    })
+})
+
+$('.minus-cart').click(function () {
+    var id = $(this).attr("pid").toString();
+    var eml = this.parentNode.children[2]
+    // console.log(id)
+    $.ajax({
+        type: 'GET',
+        url: '/minus_cart',
+        data: {
+            prod_id: id
+        },
+        success: function (data) {
+            eml.innerText = data.quantity
+            document.getElementById("amount").innerText = data.amount
+            document.getElementById("total").innerText = data.total
+
+        }
+    })
+})
+
+$('.remove-cart').click(function () {
+    var id = $(this).attr("pid").toString();
+    var eml = this
+    // console.log(id)
+    $.ajax({
+        type: 'GET',
+        url: '/remove_cart',
+        data: {
+            prod_id: id
+        },
+        success: function (data) {
+
+            document.getElementById("amount").innerText = data.amount
+            document.getElementById("total").innerText = data.total
+            eml.parentNode.parentNode.parentNode.parentNode.remove()
+
+        }
+    })
+})
--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
 from django.contrib import messages
+from django.db.models import Q
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
@@ -59,6 +61,80 @@ def show_cart(request):
             return render(request, 'app/addtocart.html', context)
         else:
             return render(request, 'app/emptycart.html')
+
+
+def plus_cart(request):
+    if request.method == "GET":
+        prod_id = request.GET['prod_id']
+        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.quantity += 1
+        c.save()
+
+        amount = 0.0
+        total = 0.0
+        shipping = 50.0
+        cart_product = [p for p in Cart.objects.all() if p.user == request.user]
+
+        for p in cart_product:
+            temp_amount = (p.quantity * p.product.discount_price)
+            amount += temp_amount
+        total = amount + shipping
+
+        data = {
+            'quantity': c.quantity,
+            'amount': amount,
+            'total': total,
+
+        }
+        return JsonResponse(data)
+
+
+def minus_cart(request):
+    if request.method == "GET":
+        prod_id = request.GET['prod_id']
+        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.quantity -= 1
+        c.save()
+
+        amount = 0.0
+        total = 0.0
+        shipping = 50.0
+        cart_product = [p for p in Cart.objects.all() if p.user == request.user]
+
+        for p in cart_product:
+            temp_amount = (p.quantity * p.product.discount_price)
+            amount += temp_amount
+        total = amount + shipping
+
+        data = {
+            'quantity': c.quantity,
+            'amount': amount,
+            'total': total,
+
+        }
+        return JsonResponse(data)
+
+
+def remove_cart(request):
+    if request.method == "GET":
+        prod_id = request.GET['prod_id']
+        c = Cart.objects.get(Q(product=prod_id) & Q(user=request.user))
+        c.delete()
+        amount = 0.0
+        total = 0.0
+        shipping = 50.0
+        cart_product = [p for p in Cart.objects.all() if p.user == request.user]
+        for p in cart_product:
+            temp_amount = (p.quantity * p.product.discount_price)
+            amount += temp_amount
+        total = amount + shipping
+
+        data = {
+            'amount': amount,
+            'total': total,
+
+        }
+        return JsonResponse(data)
 
 
 def buy_now(request):
