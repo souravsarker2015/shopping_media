7533ef44e4769b8ae0bde216fdb1d936a009438e
sourov-sarker8251 <souravsarker2015@gmail.com>
2022-02-27 21:14:14 +0600

    updated

--- shopping_media/settings.py
+++ shopping_media/settings.py
@@ -59,26 +59,24 @@ AUTHENTICATION_BACKENDS = [
 WSGI_APPLICATION = 'shopping_media.wsgi.application'
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'shopping_media',
-        'USER': 'root',
-        'PASSWORD': '1234',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
-# Password validation
-# https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'shopping_media',
+#         'USER': 'root',
+#         'PASSWORD': '1234',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
